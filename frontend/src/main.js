54b8d626ef5d7f7be3b8fe11fba16c3f14cdf0e1
ItsJeffDev <jeffkolinmiranda14@gmail.com>
2026-07-22 10:06:48 +0800

    feat: update navbar responsive mobile layout

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,10 @@ import './style.css'
 import App from './App.vue'
 import router from "./routes/index.js";
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
 createApp(App).use(router).mount('#app')
 
 
