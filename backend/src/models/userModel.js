46c281d701d1c8b24e576640ed861ea53b3d43c2
ItsJeffDev <jeffkolinmiranda14@gmail.com>
2026-09-16 08:49:43 +0800

    chore: add prettier formatting

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -2,27 +2,20 @@ const db = require("../config/db");
 const express = require("express");
 const router = express.Router();
 
-const {
-    register,
-    login
-} = require("../controllers/authController");
-
+const { register, login } = require("../controllers/authController");
 
 const getAllUsers = async () => {
-    const [results] = await db.query("SELECT * FROM users");
-    return results;
+  const [results] = await db.query("SELECT * FROM users");
+  return results;
 };
 
 // Register
 router.post("/register", register);
 
-
 // Login
 router.post("/login", login);
 
-
 module.exports = {
-    getAllUsers,
-    router
+  getAllUsers,
+  router,
 };
-
